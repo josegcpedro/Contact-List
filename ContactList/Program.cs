39f5cdeb326737362c8659bf505498728d95eea2
Jose <pedrojcunha7@hotmail.com>
2025-01-10 10:18:46 +0100

    feat: add interactive menu with choises

--- ContactList/Program.cs
+++ ContactList/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.WriteLine("Bienvenue dans votre liste de contact!");
+        Console.WriteLine("Donnez votre nom pour commencer!");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Bonjour {name}, que voulez-vous faire");
+
+        Console.WriteLine("1. Ajouter un contact");
+        Console.WriteLine("2. Supprimer un contact");
+        Console.WriteLine("3. Modifier un contact");
+        Console.WriteLine("4. Retour");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
