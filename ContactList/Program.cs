61e2032cb24a484041b56a693a53812b315b30a4
Jose <pedrojcunha7@hotmail.com>
2025-01-10 10:25:16 +0100

    feat: add show accounts option

--- ContactList/Program.cs
+++ ContactList/Program.cs
@@ -18,19 +18,24 @@ class Program
         Console.WriteLine("1. Ajouter un contact");
         Console.WriteLine("2. Supprimer un contact");
         Console.WriteLine("3. Modifier un contact");
-        Console.WriteLine("4. Quitter");
+        Console.WriteLine("4. Afficher les contacts");
+        Console.WriteLine("5. Quitter");
 
         string choix = Console.ReadLine();
 
-        switch(choix){
+        switch (choix)
+        {
             case "1":
-                //ShowContacts();
+                //AddContacts();
                 break;
             case "2":
                 //DeleteAccount();
                 break;
             case "3":
                 //ModifyAccount();
+                break;
+            case "4":
+                //ShowAccounts();
                 break;
             default:
                 break;
