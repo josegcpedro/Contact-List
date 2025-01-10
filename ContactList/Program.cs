23cf01713b15891047deb6054853763ed3d58cb9
Jose <pedrojcunha7@hotmail.com>
2025-01-10 10:22:55 +0100

    feat: set up switch case for multiple choises

--- ContactList/Program.cs
+++ ContactList/Program.cs
@@ -18,7 +18,23 @@ class Program
         Console.WriteLine("1. Ajouter un contact");
         Console.WriteLine("2. Supprimer un contact");
         Console.WriteLine("3. Modifier un contact");
-        Console.WriteLine("4. Retour");
+        Console.WriteLine("4. Quitter");
+
+        string choix = Console.ReadLine();
+
+        switch(choix){
+            case "1":
+                //ShowContacts();
+                break;
+            case "2":
+                //DeleteAccount();
+                break;
+            case "3":
+                //ModifyAccount();
+                break;
+            default:
+                break;
+        }
     }
 
 
